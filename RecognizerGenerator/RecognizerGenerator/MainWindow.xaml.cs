3f57362fca840e7f788b570500563c2a394e7b42
Boris <boralgladyshev@gmail.com>
2023-05-16 01:09:04 +0300

    Продолжение попыток сделать интерфейс

--- RecognizerGenerator/RecognizerGenerator/MainWindow.xaml.cs
+++ RecognizerGenerator/RecognizerGenerator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,18 +22,22 @@ namespace RecognizerGenerator
   /// </summary>
   public partial class MainWindow : Window
   {
-    public List<InputSymbol> InputSymbols { get; set; } = new() { new("a"), new("b") };
-    public List<MachineState> States { get; set; } = new() { new("N"), new("E"), new("B") };
+    //public ObservableCollection<InputSymbol> InputSymbols { get; set; } = new() { new("a"), new("b") };
+    //public ObservableCollection<MachineState> States { get; set; } = new() { new("N"), new("E"), new("B") };
+    //public MachineState[,] TransitionTable { get; set; } = new MachineState[2, 3] { { new("DICK"), new(), new() }, { new(), new(), new() } };
 
     public MainWindow()
     {
+      //DataContext = this;
       InitializeComponent();
-      DataContext = this;
     }
 
     private void GenerateButton_Click(object sender, RoutedEventArgs e)
     {
-
+      for (int i = 0; i < TransitionStatesDataGrid.Columns.Count; i++)
+      {
+        TransitionStatesDataGrid.Columns[i] = new DataGridTemplateColumn() { CellTemplate = (DataTemplate)Resources["DataTemplate_Level2"] };
+      }
     }
   }
 }
---
+++ RecognizerGenerator/RecognizerGenerator/ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecognizerGenerator
+{
+  public class ViewModel
+  {
+    public ObservableCollection<InputSymbol> InputSymbols { get; set; } = new() { new("a"), new("b") };
+    public ObservableCollection<MachineState> States { get; set; } = new() { new("N"), new("E"), new("B") };
+    public MachineState[,] TransitionTable { get; set; } = new MachineState[3, 2] { { new("DICK"), new() }, { new(), new() }, { new(), new() } };
+  }
+}
